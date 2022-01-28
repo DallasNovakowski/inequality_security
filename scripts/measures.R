398a139aab9b497be37f591abb8d75b8859c2b86
Dallas Novakowski <47903855+DallasNovakowski@users.noreply.github.com>
2022-01-28 12:27:25 -0800

    add measures file

---
+++ scripts/measures.R
@@ -0,0 +1,29 @@
+#before security decision
+pre_partner_know_text <- "I know whether my partner will try stealing from me"
+
+pre_partner_attempt_text <- "My partner is probably going to try stealing from me"
+pre_success_prob_text <- "If my partner tries to steal from me, they'll probably succeed"
+
+fair_game_text <- "This game is fair"
+
+fair_distribution_text <- "The way incomes were given for this game is fair"
+deserved_income_text <- "I deserve my income for this game"
+deserved_role_text <- "I deserve my role for this game"
+reasonable_income_text <- "My partner's assigned income is reasonable"
+
+guilty_text <- "I feel guilty for my position in this game"
+want_money_text <- "In this game, I want to try to keep as much of my money as possible"
+
+partner_fair_text <- "My partner probably thinks this game is fair"
+partner_admiration_text <- "My partner probably admires me"
+
+partner_envy_text <- "My partner probably feels envious of me"
+partner_jealous_text <- "My partner probably feels jealous of me"
+partner_frustrated_text <- "My partner probably feels frustrated"
+partner_bitter_text <- "My partner probably feels bitter"
+
+
+
+# post game
+post_partner_attempt_text <- "My partner is probably going to try stealing from me"
+post_success_prob_text <- "If my partner tries to steal from me, they'll probably succeed"
